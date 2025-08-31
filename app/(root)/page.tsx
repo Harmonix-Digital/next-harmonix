048586eb5fdd4566ce6c4009472d0a849930a4db
coder-mahmud <mahmud.linked@gmail.com>
2025-08-31 18:37:16 +0600

    Home page half done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import HomeThreeAgents from "@/components/home/HomeThreeAgents";
 
 export default function Home() {
   return (
@@ -56,8 +57,9 @@ export default function Home() {
         <div className="container">
           <h2 className="text-big font-semibold mb-9 text-center mb-11">Design a business that runs <span className="text-golden">itself</span> </h2>
           <p className="sub_text text-2xl text-center">A full AI operating system for teams who value freedom, clarity, and flow.</p>
+          
 
-          <div className="home_desc_blocks_wrap flex gap-6 mt-19">
+          <div className="home_desc_blocks_wrap flex flex-col lg:flex-row gap-6 mt-19">
             
             <div className="home_desc_block rounded-3xl bg-[#D9D9D91A]  pt-0 pb-9">
               <div className="flex justify-end">
@@ -167,6 +169,195 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="home_harmonix_can_do mt-19">
+        <div className="container">
+          <h2 className="text-big font-semibold text-center mb-11 text-golden">What Can Harmonix Agents Do?</h2>
+          <p className="sub_text text-2xl text-center">Discover the specialized AI agents that will transform your business operations.</p>
+
+          <HomeThreeAgents />
+        </div>
+
+        
+
+        
+      </section>
+
+
+      <section className="home_tiers mt-19">
+        <div className="container">
+          
+          <div className="sec_btn flex items-center justify-center gap-2 bg-[#D9D9D9]/10 rounded-[20px] px-8 py-6 w-[296px] mx-auto text-golden text-lg font-semibold">
+            <span>Early Adopter Pricing</span>
+            <Image src="/images/clock.png" alt="Clock icon" width={28} height={28} />
+          </div>
+
+          <h2 className="text-big font-semibold text-center mb-5 text-golden">Investment Tiers
+          </h2>
+          <p className="sub_text text-2xl text-center">Limited time offer - 30% discount. Secure your spot before rates increase.</p>
+
+          <div className="tiers_wrap flex flex-col md:flex-row md:flex-wrap gap-4 items-center justify-center mt-24">
+            
+            <div className="tier bg-[#D9D9D9]/10 rounded-[10px] flex flex-col items-center p-6">
+              <p className="text-golden text-2xl font-semibold mb-4">Starter</p>
+              <p className="font-semibold text-[12px] mb-6">1 Agent</p>
+              <p className="text-offwhite text-[16px] font-semibold mb-2">$320/month</p>
+              <p className="text-golden text-[9px] font-semibold mb-6">Ongoing monthly cost</p>
+              <p className="flex gap-2 items-center">
+                <span className="line-through text-[8px]">$4.4,000</span>
+                <span className="text-golden text-[12px] mb-2">30% OFF</span>
+              </p>
+              <p className="text-[12px]">$4.4K setup</p>
+              <p className="text-golden text-[8px] font-semibold mb-6">One-time platform setup</p>
+
+              <button className="rounded-[8px] px-2 py-3 border border-golden text-[10px]">Pay 30% Deposit & Get 15% Off + Priority Access</button>
+            </div>
+
+            <div className="tier bg-[#D9D9D9]/10 rounded-[10px] flex flex-col items-center p-6 relative">
+              <div className="popular bg-[#D9D9D9]/10 rounded-[12px] px-2.5 py-3.5 absolute -translate-y-1/2 -top-[10px]">
+                <p className="text-[10px] text-golden font-semibold">Most Popular</p>  
+              </div>
+              <p className="text-golden text-2xl font-semibold mb-4">Growth</p>
+              <p className="font-semibold text-[12px] mb-6">3–5 Agents</p>
+              <p className="text-offwhite text-[16px] font-semibold mb-2">$960 – $1,600/month</p>
+              <p className="text-golden text-[9px] font-semibold mb-6">Ongoing monthly cost</p>
+              <p className="flex gap-2 items-center">
+                <span className="line-through text-[8px]">$8,000</span>
+                <span className="text-golden text-[12px] mb-2">30% OFF</span>
+              </p>
+              <p className="text-[12px]">$8K setup</p>
+              <p className="text-golden text-[8px] font-semibold mb-6">One-time platform setup</p>
+
+              <button className="rounded-[8px] px-2 py-3 border border-golden text-[10px]">Pay 30% Deposit & Get 15% Off + Priority Access</button>
+            </div>  
+
+            <div className="tier bg-[#D9D9D9]/10 rounded-[10px] flex flex-col items-center p-6">
+              <p className="text-golden text-2xl font-semibold mb-4">Expansion</p>
+              <p className="font-semibold text-[12px] mb-6">6–8 Agents</p>
+              <p className="text-offwhite text-[16px] font-semibold mb-2">$1,920 – $2,560/month
+              </p>
+              <p className="text-golden text-[9px] font-semibold mb-6">Ongoing monthly cost</p>
+              <p className="flex gap-2 items-center">
+                <span className="line-through text-[8px]">$15,000</span>
+                <span className="text-golden text-[12px] mb-2">30% OFF</span>
+              </p>
+              <p className="text-[12px]">$15K setup</p>
+              <p className="text-golden text-[8px] font-semibold mb-6">One-time platform setup</p>
+
+              <button className="rounded-[8px] px-2 py-3 border border-golden text-[10px]">Pay 30% Deposit & Get 15% Off + Priority Access</button>
+            </div>
+
+            <div className="tier bg-[#D9D9D9]/10 rounded-[10px] flex flex-col items-center p-6">
+              <p className="text-golden text-2xl font-semibold mb-4">Enterprise</p>
+              <p className="font-semibold text-[12px] mb-6">9–12 Agents</p>
+              <p className="text-offwhite text-[16px] font-semibold mb-2">$2,880 – $3,840/month              </p>
+              <p className="text-golden text-[9px] font-semibold mb-6">Ongoing monthly cost</p>
+              <p className="flex gap-2 items-center">
+                <span className="line-through text-[8px]">$21,500+</span>
+                <span className="text-golden text-[12px] mb-2">30% OFF</span>
+              </p>
+              <p className="text-[12px]">$21.5K+ setup</p>
+              <p className="text-golden text-[8px] font-semibold mb-6">One-time platform setup</p>
+
+              <button className="rounded-[8px] px-2 py-3 border border-golden text-[10px]">Pay 30% Deposit & Get 15% Off + Priority Access</button>
+            </div>
+
+
+
+          </div>
+
+
+
+
+        </div>
+      </section>
+
+      <section className="owner_feature mt-19">
+        <div className="container">
+
+          <div className="sec_btn flex items-center justify-center gap-2 bg-[#D9D9D9]/10 rounded-[20px] px-8 py-6 w-[296px] mx-auto text-golden text-lg font-semibold">
+            <span>Harmonix Tokens</span>
+            <Image src="/images/dollar.png" alt="Clock icon" width={28} height={28} />
+          </div>
+
+          <h2 className="text-big font-semibold text-center mb-5 text-golden"> Unlock the Future of AI Ownership</h2>
+          <p className="sub_text text-2xl text-center">Our NFTs aren’t just collectibles. They’re functional tokens designed to connect you with rewards, access, and growth inside our AI-driven platform</p>
+
+          <div className="flex">
+            <div className="relative flex">
+              
+              <div>
+                <div className="single_card bg-[#D9D9D9]/10 rounded-[10px] px-3.5 py-2 relative -right-[15px]">
+                  <Image src="/images/token-img.png" width={258} height={261} alt="ownership image" />
+                  <p className="text-golden font-semibold">Harmonix Tokens 2</p>
+                  <div className="flex justify-between">
+                    
+                    <div className="flex gap-1 items-center">
+                      <Image src="/images/token-icon.png" alt="token icon" width={20} height={20} />
+                      <span className="text-sm font-semibold">12.5 ETH</span>
+                    </div>
+
+                    <div className="flex gap-1 items-center">
+                      <Image className="w-3 h-3" src="/images/clock-white.png" alt="token icon" width={12} height={12} />
+                      <span className="text-sm font-semibold">6h:30m:7s</span>
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="single_card bg-[#D9D9D9]/10 rounded-[10px] px-3.5 py-2 relative top-[70%] ">
+                  <Image src="/images/token-img.png" width={258} height={261} alt="ownership image" />
+                  <p className="text-golden font-semibold">Harmonix Tokens 2</p>
+                  <div className="flex justify-between">
+                  
+                    <div className="flex gap-1 items-center">
+                      <Image src="/images/token-icon.png" alt="token icon" width={20} height={20} />
+                      <span className="text-sm font-semibold">12.5 ETH</span>
+                    </div>
+
+                    <div className="flex gap-1 items-center">
+                      <Image className="w-3 h-3" src="/images/clock-white.png" alt="token icon" width={12} height={12} />
+                      <span className="text-sm font-semibold">6h:30m:7s</span>
+                    </div>
+
+
+                  </div>
+
+
+
+                </div>          
+
+              </div>
+
+              <div>
+                <div className="single_card bg-[#D9D9D9]/10 rounded-[10px] px-3.5 py-2 relative -left-[15px]">
+                  <Image src="/images/token-img.png" width={258} height={261} alt="ownership image" />
+                  <p className="text-golden font-semibold">Harmonix Tokens 2</p>
+                  <div className="flex justify-between">
+                    
+                    <div className="flex gap-1 items-center">
+                      <Image src="/images/token-icon.png" alt="token icon" width={20} height={20} />
+                      <span className="text-sm font-semibold">12.5 ETH</span>
+                    </div>
+
+                    <div className="flex gap-1 items-center">
+                      <Image className="w-3 h-3" src="/images/clock-white.png" alt="token icon" width={12} height={12} />
+                      <span className="text-sm font-semibold">6h:30m:7s</span>
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+        </div>
+      </section>
 
     </>
   );
---
+++ components/home/HomeThreeAgents.tsx
@@ -0,0 +1,241 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const agents = [
+  {
+    id: "sales",
+    title: "Sales Rep Agent",
+    desc: "Books, follows up, and closes for you.",
+    time: "22 hrs/week",
+    cost: "$320/month",
+    token: "50K",
+    integrations: ["GHL", "Slack", "HubSpot"],
+    capabilities: [
+      "Lead qualification",
+      "Deal closing",
+      "Follow-up automation",
+      "Pipeline management",
+    ],
+    deploy:"/",
+    learnMore:"/"
+  },
+  {
+    id: "sop",
+    title: "SOP Architect",
+    desc: "Turns chaos into clean workflows.",
+    time: "18 hrs/week",
+    cost: "$240/month",
+    token: "60K",
+    integrations: ["Google", "Slack", "HubSpot"],
+    capabilities: [
+      "Lead qualification",
+      "Deal closing",
+      "Follow-up automation",
+      "Pipeline management",
+    ],
+    deploy:"/",
+    learnMore:"/"
+  },
+  {
+    id: "content",
+    title: "Content Producer",
+    desc: "Auto-creates, schedules, and posts with tone-match.",
+    time: "25 hrs/week",
+    cost: "$260/month",
+    token: "40K",
+    integrations: ["Facebook", "Slack", "HubSpot"],
+    capabilities: [
+      "Lead qualification",
+      "Deal closing",
+      "Follow-up automation",
+      "Pipeline management",
+    ],
+    deploy:"/",
+    learnMore:"/"
+  },
+];
+
+
+export default function HomeThreeAgents() {
+
+  const [selected, setSelected] = useState("sales");
+
+  const current = agents.find((a) => a.id === selected)!;
+
+  const topPosition = selected == 'sales' ? 2 : selected == 'sop' ? 30 : 75
+
+  return (
+    <>
+      <div className="hidden xl:flex justify-center py-12">
+        {/* Left column */}
+        <div className="flex flex-col gap-19 col-span-3 max-w-[346px]">
+          {agents.map((agent) => (
+            
+            <button
+              key={agent.id}
+              onClick={() => setSelected(agent.id)}
+              className={`rounded-xl px-6 py-9 text-left  transition home_agent_index border-[0.7px] bg-[#D9D9D9]/10  ${
+                selected === agent.id
+                  ? "border-golden"
+                  : "border-[#095552CF]"
+              }`}
+            >
+              <h3 className="font-semibold text-golden text-2xl mb-4">{agent.title}</h3>
+              <p className="">{agent.desc}</p>
+              <div className="flex justify-between gap-5 mt-9">
+                <div className="">
+                  <p className="text-golden text-sm font-meidum mb-1">Time Saved</p>
+                  <p className="text-sm">{agent.time}</p>
+                </div>
+
+                <div className="">
+                  <p className="text-golden text-sm font-meidum mb-1">Monthly</p>
+                  <p className="text-sm">{agent.cost}</p>
+                </div>
+              </div>
+              
+            </button>
+
+          ))}
+        </div>
+
+        {/* Middle indicator */}
+        <div className=" relative  col-span-2 w-full max-w-[280px]">
+          <div className="flex flex-col gap-20 h-full absolute left-1/2 -translate-x-1/2">
+            {agents.map((agent) => (
+              <div key={agent.id} className="line w-[1px] bg-white h-1/4" style={{ background: agent.id === selected ? "#C19E5B" : "linear-gradient(90deg, #F5F5F5 0%, #13BBB4 100%)" }}>
+
+              </div>
+            ))}
+          </div>
+
+
+          <Image className="absolute left-1/2 -translate-x-1/2  transition-all duration-500"
+            style={{
+              top: `${topPosition}%`,
+            }} src="/images/selected-glow.png" alt="Selected Glow" width={180} height={160}  />
+        </div>
+
+        {/* Right column */}
+        <div className="rounded-xl border border-gray-700 bg-black/40 pt-19 pb-15 px-14 col-span-4 w-full max-w-[576px]">
+          <h2 className="text-golden text-4xl mb-11 font-semibold">
+            {current.title}
+          </h2>
+          <p className="text-2xl font-semibold">{current.desc}</p>
+
+          <div className="flex gap-6 justify-between mt-11">
+            
+            <div>
+              <p className="text-2xl font-medium text-center">{current.cost}</p>
+              <p className="text-golden text-lg text-center ">Monthly Cost</p>
+            </div>          
+            <div>
+              <p className="text-2xl font-medium text-center">{current.token}</p>
+              <p className="text-golden text-lg text-center ">Token Usage (Moderate)</p>
+            </div>          
+            <div>
+              <p className="text-2xl font-medium text-center">{current.cost}</p>
+              <p className="text-golden text-lg text-center ">Time Saved</p>
+            </div>
+
+            
+          </div>
+
+          <div>
+            <p className="text-golden text-xl font-medium">Integrations:</p>
+            <div className="flex gap-15 mt-6">
+              {current.integrations.map((item,idx) => (
+                <button key={idx} className="border border-golden rounded-[40px] px-7 py-3" >{item}</button>
+              ))}
+              
+            </div>
+          </div>
+
+          <div className="mt-11">
+            <h4 className="text-2xl font-semibold mb-6">Key Capabilities:</h4>
+            <ul className="list-disc list-inside text-golden text-lg font-semibold columns-2">
+              {current.capabilities.map((cap, i) => (
+                <li key={i} className="flex gap-1 items-center mb-4"><Image src="/images/circle-tick.png" alt="tich mark" width={17} height={17} />{cap}</li>
+              ))}
+            </ul>
+          </div>
+
+
+          <div className="mt-11 flex gap-6 justify-center">
+            <Link className="text-2xl font-semibold bg-golden rounded-[20px]  p-6 text-siteBlack border border-golden hover:bg-siteBlack hover:text-golden transition-all duration-300" href={current.deploy}>Deploy Agent</Link>
+
+            <Link className="text-2xl font-semibold flex gap-2 items-center rounded-[20px] p-6 border border-golden text-offwhite hover:bg-golden hover:text-siteBlack transition-all duration-300" href={current.learnMore}><span>Learn More</span> <Image src="/images/angle-right.png" alt="Right angle icon" width={20} height={20} /> </Link>
+          </div>
+
+
+
+
+
+        </div>
+      </div>
+
+      <div className="py-12 flex xl:hidden flex-col gap-14 justify-center items-center">
+          {agents.map(agent => (
+            <div key={agent.id} className="rounded-xl border border-gray-700 bg-black/40 pt-19 pb-15 px-14 col-span-4 w-full max-w-[576px]">
+            <h2 className="text-golden text-4xl mb-11 font-semibold">
+              {current.title}
+            </h2>
+            <p className="text-2xl font-semibold">{agent.desc}</p>
+
+            <div className="flex gap-6 justify-between mt-11">
+              
+              <div>
+                <p className="text-2xl font-medium text-center">{agent.cost}</p>
+                <p className="text-golden text-lg text-center ">Monthly Cost</p>
+              </div>          
+              <div>
+                <p className="text-2xl font-medium text-center">{agent.token}</p>
+                <p className="text-golden text-lg text-center ">Token Usage (Moderate)</p>
+              </div>          
+              <div>
+                <p className="text-2xl font-medium text-center">{agent.cost}</p>
+                <p className="text-golden text-lg text-center ">Time Saved</p>
+              </div>
+
+              
+            </div>
+
+            <div>
+              <p className="text-golden text-xl font-medium">Integrations:</p>
+              <div className="flex gap-15 mt-6">
+                {agent.integrations.map((item,idx) => (
+                  <button key={idx} className="border border-golden rounded-[40px] px-7 py-3" >{item}</button>
+                ))}
+                
+              </div>
+            </div>
+
+            <div className="mt-11">
+              <h4 className="text-2xl font-semibold mb-6">Key Capabilities:</h4>
+              <ul className="list-disc list-inside text-golden text-lg font-semibold columns-2">
+                {agent.capabilities.map((cap, i) => (
+                  <li key={i} className="flex gap-1 items-center mb-4"><Image src="/images/circle-tick.png" alt="tich mark" width={17} height={17} />{cap}</li>
+                ))}
+              </ul>
+            </div>
+
+
+            <div className="mt-11 flex  gap-6 justify-center">
+              <Link className="text-2xl font-semibold bg-golden rounded-[20px]  p-6 text-siteBlack border border-golden hover:bg-siteBlack hover:text-golden transition-all duration-300" href={current.deploy}>Deploy Agent</Link>
+
+              <Link className="text-2xl font-semibold flex gap-2 items-center rounded-[20px] p-6 border border-golden text-offwhite hover:bg-golden hover:text-siteBlack transition-all duration-300" href={current.learnMore}><span>Learn More</span> <Image src="/images/angle-right.png" alt="Right angle icon" width={20} height={20} /> </Link>
+            </div>
+
+
+
+
+
+          </div>            
+          ))}
+      </div>
+    </>
+  );
+}
